e792beac520423dd24033fcb92c486ef1596b9f0
Long Nguyen Hoang <hoanglongdev533@gmail.com>
2024-06-23 08:56:44 +0700

    second init

---
+++ src/lib/actions/user.action.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import connect from "../db";
+import User from "../models/user";
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error: any) {
+    console.log(error);
+  }
+}
