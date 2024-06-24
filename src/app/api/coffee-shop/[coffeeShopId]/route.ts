bc8c3799b587ccf593ba0333084158485d9e4772
Long Nguyen Hoang <hoanglongdev533@gmail.com>
2024-06-24 17:20:51 +0700

    add get coffee shop by id api

---
+++ src/app/api/coffee-shop/[coffeeShopId]/route.ts
@@ -0,0 +1,34 @@
+import connect from "@/lib/db";
+import CoffeeShop from "@/lib/models/coffeeShop";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, context: { params: any }) {
+  try {
+    const { coffeeShopId } = context.params.coffeeShopId;
+
+    if (!coffeeShopId || !Types.ObjectId.isValid(coffeeShopId)) {
+      return NextResponse.json(
+        { message: "Invalid coffeeShopId" },
+        { status: 400 }
+      );
+    }
+
+    await connect();
+    const coffeeShop = CoffeeShop.findById(coffeeShopId);
+
+    if (!coffeeShop) {
+      return NextResponse.json(
+        { message: "Coffee Shop does not exist" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(coffeeShop, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error while creating feedback " + error },
+      { status: 500 }
+    );
+  }
+}
